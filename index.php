39f8fd66150f92714ef6e5d0db0bbc160ba259a4
ovhlover <112505985+ovhlover@users.noreply.github.com>
2022-09-12 12:58:03 +0200

    Add files via upload
    Simple code for Website / Landing page made with <3 by LEMIII. www.ovhlover.me / discord.gg/1337eu

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link id="dark-mode-custom-link" rel="stylesheet" type="text/css" />
+    <link id="dark-mode-general-link" rel="stylesheet" type="text/css" />
+    <link href="style.css" rel="stylesheet" /><meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="gods of ddos com." property="og:description">
+    <script src="chrome-extension://mooikfkahbdckldjjndioackbalphokd/assets/prompt.js"></script>
+    <meta content="ovhlover.me" property="og:title">
+    <style id="dark-mode-custom-style" lang="en" type="text/css">
+    </style>
+    <style id="dark-mode-native-style" lang="en" type="text/css">
+    </style>
+    <style id="dark-mode-native-sheet" lang="en" type="text/css">
+    </style>
+    <title>ovh lovers</title>
+    </head>
+<body oncontextmenu="return false">
+<p>
+<audio autoplay=""><source src="Chief_Keef_Love_Sosa.mp3" type="audio/mpeg" /></audio>
+</p>
+
+<div id="enter"><b class="begin">Enter to site</b></div>
+
+<div id="main"></div>
+
+<p>
+<audio id="audio" src="Chief_Leef_Love_Sosa.mp3"></audio>
+<script src="audio.js"></script></p>
+
+<div class="dot"></div>
+
+<pre class="text">
+
+
+              _                          
+             | |                         
+__      _____| | ___ ___  _ __ ___   ___ 
+\ \ /\ / / _ \ |/ __/ _ \| &#39;_ ` _ \ / _ \
+ \ V  V /  __/ | (_| (_) | | | | | |  __/
+  \_/\_/ \___|_|\___\___/|_| |_| |_|\___|
+                                         
+                                         
+
+    
+    
+    
+    
+    
+    
+    stresser: <a href="https://t.me/ovhlovers">link</a> coded by: null &amp; catnip
+          </pre>
+
+<pre class="logo">
+
+        _   _                             _     
+        (_) | |                           | |    
+         _  | | _____   _____    _____   _| |__  
+        | | | |/ _ \ \ / / _ \  / _ \ \ / / &#39;_ \ 
+        | | | | (_) \ V /  __/ | (_) \ V /| | | |
+               |_| |_|\___/ \_/ \___|  \___/ \_/ |_| |_|       
+        
+<a href="https://info.flagcounter.com/THGh"><img src="https://s11.flagcounter.com/count2/THGh/bg_404040/txt_00FF08/border_00CC1B/columns_5/maxflags_20/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a>   
+     </pre>
+
+<pre class="text">
+y&#39;all welcome [ ovh lovers ]</pre>
+
+<pre class="side">
+
+</pre>
+
+<pre class="text" style="font-size: 15px;">
+<a href="plans.php">Click to See Plans</a></pre>
+information:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; members:
+
+<p><a href="https://t.me/ovhlovers">/group</a>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<a href="https://t.me/ovhlover">@null</a></p>
+
+<p></p>
+
+<p><a href="https://ovh.com">/ovh</a>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a href="https://t.me/queered">@catnip</a></p>
+
+<p></p>
+
+<p><a href="https://discord.gg/1337eu">/discord</a>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <a href="https://t.me/loodak1337">@ping</a></p>
+
+<p></p>
+
+<div aria-hidden="true" class="snowflakes">
+<div class="snowflake"></div>
+100% DDoS Guard
+
+<div class="snowflake"></div>
+100% Uptime
+
+<div class="snowflake"></div>
+discord.gg/1337eu
+
+<div class="snowflake"></div>
+</div>
+</body>
+</html>
---
+++ plans.php
@@ -0,0 +1,73 @@
+<?php include("includes/settings.php") ?>
+<html>
+    <head>
+        <title><?= $site_name ?> - Plans</title>
+        <link rel="stylesheet" href="planstyle.css">
+        <link rel="stylesheet" href="planstyle2.css">
+        <script src="https://kit.fontawesome.com/d394b6a666.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="unhittable-gray-bg">
+        <div class="backButton">
+            <a href="index.php">Back to Main Page</a>
+        </div>
+        <pre class="logo" style="padding: 50px;">
+        _                 
+  _ __ | | __ _ _ __  ___ 
+ | '_ \| |/ _` | '_ \/ __|
+ | |_) | | (_| | | | \__ \
+ | .__/|_|\__,_|_| |_|___/
+ |_|                      
+
+</pre>
+  <section id="pluto" class="unhittable-gray-bg">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-6">
+          <div class="unhittable-iconbox unhittable-style2">
+            <div class="unhittable-iconbox-icon" style="color: #7bff00;"><i class="fas fa-user-tie"></i></div>
+            <h2 class="unhittable-iconbox-title"> <strong style="color: #7bff00;">BRONZE $15</strong></h2>
+            <div class="unhittable-section-heading unhittable-style1">
+              <h4 class="unhittable-section-heading-title"></h4>
+            </div>
+            <ul>Duration: <b style="color: #7bff00;">Monthly</b></ul>
+            <ul>Concurrents: <b style="color: #7bff00;">1</b></ul>
+            <ul>Time: <b style="color: #7bff00;">100s</b></ul>
+            <ul>VIP: <b style="color: #7bff00;">False</b></ul>
+            <br><a href="https://discord.gg/TxmENp7gRm" class="unhittable-btn unhittable-style2 font-weight-bold">Buy Now!</a>
+          </div>
+          <div class="unhittable-height-b30 unhittable-height-lg-b30"></div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="unhittable-iconbox unhittable-style2">
+            <div class="unhittable-iconbox-icon" style="color: #7bff00;"><i class="fas fa-user-tie"></i></div>
+            <h2 class="unhittable-iconbox-title"> <strong style="color: #7bff00;">GOLD $30</strong></h2>
+            <div class="unhittable-section-heading unhittable-style1">
+              <h4 class="unhittable-section-heading-title"></h4>
+            </div>
+            <ul>Duration: <b style="color: #7bff00;">Monthly</b></ul>
+            <ul>Concurrents: <b style="color: #7bff00;">2</b></ul>
+            <ul>Time: <b style="color: #7bff00;">300s</b></ul>
+            <ul>VIP: <b style="color: #7bff00;">True</b></ul>
+            <br><a href="https://discord.gg/TxmENp7gRm" class="unhittable-btn unhittable-style2 font-weight-bold">Buy Now!</a>
+          </div>
+          <div class="unhittable-height-b30 unhittable-height-lg-b30"></div>
+        </div>
+        <div class="col-lg-4 col-md-6">
+          <div class="unhittable-iconbox unhittable-style2">
+            <div class="unhittable-iconbox-icon" style="color: #7bff00;"><i class="fas fa-user-tie"></i></div>
+            <h2 class="unhittable-iconbox-title"> <strong style="color: #7bff00;">PLATINUM $40</strong></h2>
+            <div class="unhittable-section-heading unhittable-style1">
+              <h4 class="unhittable-section-heading-title"></h4>
+            </div>
+            <ul>Duration: <b style="color: #7bff00;">Monthly</b></ul>
+            <ul>Concurrents: <b style="color: #7bff00;">4</b></ul>
+            <ul>Time: <b style="color: #7bff00;">500s</b></ul>
+            <ul>VIP: <b style="color: #7bff00;">True</b></ul>
+            <br><a href="https://discord.gg/TxmENp7gRm" class="unhittable-btn unhittable-style2 font-weight-bold">Buy Now!</a>
+          </div>
+        
+          
+  </section>
+  
+  
+</html>
